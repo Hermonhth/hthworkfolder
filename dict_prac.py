50617d9d636bc9f67cff75e8625a1b2a6a1c600d
Hermonhth <cyberhyper25@gmail.com>
2021-06-16 12:36:14 -0700

    adding dictionary pratice file

---
+++ dict_prac.py
@@ -0,0 +1,49 @@
+'''
+Chicken $1.59
+Beef $1.99
+Cheese $1.00
+Milk $2.50
+'''
+
+grocery_list = {
+    "Chicken": 1.59,
+    "Beef": 1.99,
+    "Cheese": 1,
+    "Milk": 2.5,
+}
+
+EPL_standings = {
+    "Chealsea": 4,
+    "Mancity": 1,
+    "Manunited": 2,
+    "Liverpool": 3,
+}
+chealsea_standing = EPL_standings["Chealsea"]
+print(chealsea_standing)
+
+EPL_standings ["Chealsea"]=7
+print(chealsea_standing)
+
+shoe_sales= {
+"Jordan13": 1,
+"Yeezy":8,
+"Foamposite":10,
+"Airmax":5,
+"SB Dunk":20,
+}
+
+shoe_sales ["SB Dunk"] = 22
+shoe_sales ["Yeezy"] =7
+shoe_sales ["Jordan13"]+=7
+shoe_sales ["Yeezy"]+=7
+shoe_sales ["Foamposite"]+=7
+shoe_sales ["Airmax"]+=7
+shoe_sales ["SB Dunk"]+=7
+shoe_sales ["Jordan13"]-=3
+shoe_sales ["Yeezy"]-=3
+shoe_sales ["Foamposite"]-=3
+shoe_sales ["Airmax"]-=3
+shoe_sales ["SB Dunk"]-=3
+print(shoe_sales)
+
+
